1f4bbd19c6b61c0cbb1dd27debb75aaa3e81e27e
Nayan Yengul <nayan.yengul@gmail.com>
2019-08-14 06:41:26 +0530

    Balance column added.

--- Data Preprocessing.py
+++ Data Preprocessing.py
@@ -17,16 +17,13 @@ training_data_wo_pdays = training_data_wo_Id.drop(['pdays'], axis=1)
 #Remove 'duration' column as it has random data
 training_data_wo_duration = training_data_wo_pdays.drop(['duration'], axis=1)
 
-#Remove 'balance' column as it has random data
-training_data_wo_balance = training_data_wo_duration.drop(['balance'], axis=1)
-
-print(training_data_wo_balance.info())
+print(training_data_wo_duration.info())
 
 #Print total number of missing values of each features
-print(training_data_wo_balance.isnull().sum())
+print(training_data_wo_duration.isnull().sum())
 
 #write data into csv
-training_data_wo_balance.to_csv("E:\\Nayan\\Sesh\\training_data_missing_val.csv", index = False)
+training_data_wo_duration.to_csv("E:\\Nayan\\Sesh\\training_data_missing_val.csv", index = False)
 
 #As per this there are no missing values in the data set
 #but, the uknown term in e.q. 'education' column is missing value
